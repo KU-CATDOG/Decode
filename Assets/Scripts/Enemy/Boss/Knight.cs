f380fff70fb9a8b9111ba50c411bf0eaf229a91b
DESKTOP-CN6KRA6\esyun <tmddbs0602@korea.ac.kr>
2022-02-07 17:14:50 +0900

    보스디버그

--- Assets/Scripts/Enemy/Boss/Knight.cs
+++ Assets/Scripts/Enemy/Boss/Knight.cs
@@ -86,32 +86,47 @@ public class Knight : Enemy
     #endregion
     private float attackSpeed;      // ( 공격횟수 / s )
     private float dmgMultiplier = 1.0f;    // 받는 데미지 배율
-    private IEnumerator[] attackRoutines;
+    private System.Func<IEnumerator>[] ShortRangeAttackRoutines;
+    private System.Func<IEnumerator>[] LongRangeAttackRoutines;
     private float attackCoefficient; // 스킬마다 적용되는 데미지, 기획나오면 적용
     private KnightLance lance;
     private Rigidbody2D rbLance;
     private bool isRushing = false;
+    private bool isLongRange = true;
+    private float longRange;
     public bool HaveLance { get; set; }
     protected override void Start()
     {
-        attackRoutines = new IEnumerator[4] { NormalAttack(), Rush(), ThrowLance(), ShortRush() };
+        ShortRangeAttackRoutines = new System.Func<IEnumerator>[2] { ShortRush, NormalAttack };
+        LongRangeAttackRoutines = new System.Func<IEnumerator>[2] { Rush, ThrowLance };
         attackSpeed = 0.5f;
         MaxHealth = Health = 50f * 3;
         AttackDamage = 10f;
-        MovementSpeed = 1f;
+        MovementSpeed = 5f;
+        MaxMovementSpeed = 10f;
         Range = 3.0f;
+        longRange = 12.0f;
         lance = GetComponentInChildren<KnightLance>();
         rbLance = lance.GetComponent<Rigidbody2D>();
+        ConnectValue(Define.ChangableValue.Hp, typeof(Enemy).GetProperty("MaxHealth"), typeof(Enemy).GetProperty("Health"));
+        ConnectValue(Define.ChangableValue.Speed, typeof(Enemy).GetProperty("MaxMovementSpeed"), typeof(Enemy).GetProperty("MovementSpeed"));
         base.Start();
+        lance.gameObject.SetActive(false);
     }
     protected override Queue<IEnumerator> DecideNextRoutine()
     {
         Queue<IEnumerator> ret = new Queue<IEnumerator>();
         if (CheckPlayer())
         {
-            if (DistToPlayer() < Range)
+            if (!isLongRange && DistToPlayer() < Range)
             {
-                ret.Enqueue(NewActionRoutine(attackRoutines[Random.Range(0, 4)]));
+                ret.Enqueue(NewActionRoutine(ShortRangeAttackRoutines[Random.Range(0,2)]()));
+                isLongRange = Random.Range(0, 2) == 0;
+            }
+            if ( isLongRange && DistToPlayer() < longRange)
+            {
+                ret.Enqueue(NewActionRoutine(LongRangeAttackRoutines[Random.Range(0,2)]()));
+                isLongRange = Random.Range(0, 2) == 0;
             }
             else
             {
@@ -160,7 +175,7 @@ public class Knight : Enemy
     {
         if (CheckPlayer())
         {
-            transform.LookAt(player.transform);
+            LookAt(player.transform.position);
             Debug.Log("Normal");
             GameManager.Instance.GetDamaged(AttackDamage);
             if(Random.value < 0.3f)
@@ -178,6 +193,7 @@ public class Knight : Enemy
     {
         if (CheckPlayer())
         {
+            Debug.Log("RUSH");
             yield return new WaitForSeconds(1.0f);
             RaycastHit2D left;
             RaycastHit2D right;
@@ -185,6 +201,7 @@ public class Knight : Enemy
             left = Physics2D.Raycast(transform.position, new Vector2(-1, 0), Mathf.Infinity, wall);
             right = Physics2D.Raycast(transform.position, new Vector2(1, 0), Mathf.Infinity, wall);
             Vector2 rushVec = left.distance > right.distance ? new Vector2(-left.distance,0) : new Vector2(right.distance,0);
+            LookDir(rushVec);
             rb.velocity = rushVec;
             isRushing = true;
             yield return new WaitForSeconds(0.9f);
@@ -203,14 +220,20 @@ public class Knight : Enemy
     {
         if (CheckPlayer())
         {
+            Debug.Log("ThrowLance");
             Vector2 throwVec = new Vector2(player.transform.position.x - transform.position.x, 0).normalized;
-            rbLance.velocity = throwVec * (5.0f);
+            LookAt(player.transform.position);
+            lance.gameObject.SetActive(true);
+            rbLance.velocity = throwVec * (10.0f);
             lance.transform.parent = null;
             HaveLance = false;
+            yield return new WaitForSeconds(2.0f);
             while (!HaveLance) // 창을 줍는 로직은 Lance에서 구현
             {
+                yield return null;
                 rb.velocity = throwVec * MovementSpeed;
             }
+            rb.velocity = Vector2.zero;
             yield return Rush();
         }
     }
@@ -223,14 +246,26 @@ public class Knight : Enemy
     {
         if (CheckPlayer())
         {
+            Debug.Log("ShortRush");
+            LookAt(player.transform.position);
             yield return new WaitForSeconds(1.0f);
             isRushing = true;
             float rushDistance = 5.0f; // 기획에 따라 변경
             float rushTime = 0.5f; // 기획에 따라 변경
-            rb.velocity = new Vector2(rushDistance / rushTime, 0);
+            rb.velocity = new Vector2(transform.right.x * rushDistance / rushTime, 0);
             yield return new WaitForSeconds(rushTime);
             isRushing = false;
             rb.velocity = Vector2.zero;
         }
+    }
+
+    private void LookAt(Vector2 dir)
+    {
+        Vector2 lookVec = dir - (Vector2)transform.position;
+        LookDir(lookVec);
+    }
+    private void LookDir(Vector2 dir)
+    {
+        transform.rotation = Quaternion.Euler(Vector3.up * (90f + -90f * dir.x / Mathf.Abs(dir.x)));
     }
 }
--- Assets/Scripts/Enemy/Boss/KnightLance.cs
+++ Assets/Scripts/Enemy/Boss/KnightLance.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class KnightLance : MonoBehaviour
 {
+    Rigidbody2D rb;
+    bool drop = false;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Knight")
+        if (!drop && collision.tag == "Wall" || collision.tag == "Floor")
+        {
+            drop = true;
+            rb.velocity = Vector2.zero;
+        }
+        if(!drop && collision.tag == "Player")
+        {
+            GameManager.Instance.GetDamaged(20);
+        }
+        Knight knight;
+        if(drop && (knight = collision.GetComponent<Knight>()))
         {
             /// 창 줍는 모션, 애니메이션 적용을 리소스 나오면 추가해야됨
             transform.parent = collision.transform;
-            collision.GetComponent<Knight>().HaveLance = true;
+            knight.HaveLance = true;
+            drop = false;
+            gameObject.SetActive(false);
         }
     }
 }
