6c218293839f0e5c07b1553eb72d78b72475061d
HyunjinLee <hyunjin1109@korea.ac.kr>
2022-02-13 13:19:08 +0900

    컴퓨터거미 추가

---
+++ Assets/Scripts/Enemy/PCSpider.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCSpider : Enemy
+{
+    protected override void Start()
+    {
+        MaxHealth = Health = 10f;
+        AttackDamage = 5f;
+        MovementSpeed = 1f;
+        MaxMovementSpeed = 10f;
+        Range = 1.5f;     // 공격 범위
+        Interval = 1.0f;
+        ConnectValue(Define.ChangableValue.Hp, typeof(Enemy).GetProperty("MaxHealth"), typeof(Enemy).GetProperty("Health"));
+        base.Start();
+    }
+
+    protected override Queue<IEnumerator> DecideNextRoutine()
+    {
+        Queue<IEnumerator> nextRoutines = new Queue<IEnumerator>();
+
+        if (CheckPlayer())
+        {
+            if (DistToPlayer() < 10f) // 플레이어가 시야 안에 들어왔다
+            {
+                if(DistToPlayer() < Range)  nextRoutines.Enqueue(NewActionRoutine(AttackRoutine(AttackDamage)));
+                else
+                {
+                    if (GetComponent<Rigidbody2D>().velocity.y >= 0)
+                        nextRoutines.Enqueue(NewActionRoutine(MoveTowardPlayer(MovementSpeed)));
+                    else nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(1f)));
+                }
+            }
+            else
+            {
+                nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(1f)));
+            }
+        }
+        else nextRoutines.Enqueue(NewActionRoutine(WaitRoutine(1f)));
+
+        return nextRoutines;
+    }
+
+    private IEnumerator AttackRoutine(float dmg)
+    {
+        if (CheckPlayer())
+        {
+            GameManager.Instance.GetDamaged(dmg);
+            yield return new WaitForSeconds(Interval);
+        }
+        else yield return null;
+    }
+}
