170b1e1a095f5bb151f642ac92b75854bc2ea327
HyunjinLee <hyunjin1109@korea.ac.kr>
2022-03-04 06:59:03 +0900

    ㅅㅅ

--- Assets/Scripts/Util/PaintBackground.cs
+++ Assets/Scripts/Util/PaintBackground.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 
 public class PaintBackground : MonoBehaviour
 {
-    public SpriteRenderer spRenderer;
     private Collider2D coll;
 
     private void OnEnable()
     {
         coll = GetComponent<BoxCollider2D>();
-        spRenderer = GetComponentInChildren<SpriteRenderer>();
-        Paint(coll);
     }
 
-    public void Paint(Collider2D coll)
-    {
-        spRenderer.transform.localPosition = new Vector3(coll.offset.x, coll.offset.y, 10);
-        //coll.offset = new Vector3(0, 0, 0);
-        spRenderer.drawMode = SpriteDrawMode.Tiled;
-        spRenderer.size = new Vector2(coll.bounds.extents.x * 2, coll.bounds.extents.y * 2);
-    }
+
 }
